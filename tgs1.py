5b2454fb04d9936366019c63a5e5e223689c8663
Ihsan Muhamad Sobari <isan.ms32@gmail.com>
2022-02-14 10:03:50 +0700

    commit tgs1

--- tgs1.py
+++ tgs1.py
@@ -1,9 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "Johanes Kalas, Carissa Ulibasa, Devi Apriliani S, Jovanka Helen, Reza Haznam"
-             
 class Player:
 	warna = ''
 	mataDadu = 0
